186daa62fdeefa3cfce06f407cc378e9344a571c
Matthew Temple <matt@mattytemple.com>
2011-04-02 23:38:19 +0100

    Added large image for latest post
    added a large image for the latest post will need styling

--- home.php
+++ home.php
@@ -1,6 +1,14 @@
 <?php get_header(); ?>
 
 		<div id="homephotos">
+			<div id="homephotolatest">
+				<?php query_posts('cat='.get_option('pd_photos_category').'&showposts=1'); ?>
+				<?php while (have_posts()) : the_post(); ?>
+				<div class="photo" style="background:url(<?php print (get_post_meta($post->ID, 'thimage_value', $single = true)) ? get_post_meta($post->ID, 'thimage_value', $single = true) : get_bloginfo('template_directory').'/img/empty.gif' ?>) center center;">
+				    <a href="<?php the_permalink() ?>" title="<?php the_title(); ?>"><img src="<?php print get_bloginfo('template_directory'); ?>/img/empty.gif" alt="<?php the_title(); ?>" width="1000" /></a>
+				</div>
+				<?php endwhile; ?>
+			</div>
             	<div id="homephotoholder">
 					<?php query_posts('cat='.get_option('pd_photos_category').'&showposts=20', 'offset=1'); ?>
                     <?php while (have_posts()) : the_post(); ?>
