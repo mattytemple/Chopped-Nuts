9c6aa06fdedc961f3c9e187fadf05a9758b6636e
Matthew Temple <matt@mattytemple.com>
2011-04-03 08:43:27 +0100

    Adjust large image height to match image size
    auto adjust height of large image box to match large image size and stop repeating the image

--- home.php
+++ home.php
@@ -3,7 +3,13 @@
 		<div id="homephotolatest">
 			<?php query_posts('cat='.get_option('pd_photos_category').'&showposts=1'); ?>
 			<?php while (have_posts()) : the_post(); ?>
-			<div class="latestphoto" style="width:9500px;height:auto;margin-left:auto;margin-right:auto;border:3px solid #F09;margin-bottom:10px;background:url(<?php print (get_post_meta($post->ID, 'big_photo_value', $single = true)) ? get_post_meta($post->ID, 'big_photo_value', $single = true) : get_bloginfo('template_directory').'/img/empty.gif' ?>) center center;">
+			<?php
+				$imagepath = get_post_meta($post->ID, 'big_photo_value', $single = true);
+				list($width, $height, $type, $attr)=getimagesize($imagepath); 
+				$ht=$height; 
+				$wd=$width;
+			?>
+			<div class="latestphoto" style="width:950px; height:<?php echo $ht; ?>px; margin-left:auto;margin-right:auto;border:3px solid #F09;margin-bottom:10px;background:url(<?php print (get_post_meta($post->ID, 'big_photo_value', $single = true)) ? get_post_meta($post->ID, 'big_photo_value', $single = true) : get_bloginfo('template_directory').'/img/empty.gif' ?>) no-repeat center center;">
 			    <a href="<?php the_permalink() ?>" title="<?php the_title(); ?>"><img src="<?php print get_bloginfo('template_directory'); ?>/img/empty.gif" alt="<?php the_title(); ?>" width="950px" height="auto" /></a>
 			</div>
 			<?php endwhile; ?>
